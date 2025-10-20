c48e377f2a4cfb7dedec681822e48aa8c6cf4b1b
Joehoel <31251240+Joehoel@users.noreply.github.com>
2025-10-20 17:52:25 +0000

    chore: fix code styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -6,7 +6,7 @@ use Saloon\Http\Faking\MockClient;
 // Ensure isolated tests and prevent real HTTP requests during tests
 uses()
     ->beforeEach(function () {
-        if (method_exists(MockClient::class, "destroyGlobal")) {
+        if (method_exists(MockClient::class, 'destroyGlobal')) {
             MockClient::destroyGlobal();
         }
     })
