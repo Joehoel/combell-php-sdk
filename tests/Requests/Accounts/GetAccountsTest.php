919ae78c5e78b4dfa0d66e6cdf0dafc451464e7e
Joehoel <31251240+Joehoel@users.noreply.github.com>
2025-10-20 20:10:55 +0000

    chore: fix code styling

--- tests/Requests/Accounts/GetAccountsTest.php
+++ tests/Requests/Accounts/GetAccountsTest.php
@@ -6,15 +6,15 @@ use Joehoel\Combell\Requests\Accounts\GetAccounts;
 use Saloon\Http\Faking\MockClient;
 use Saloon\Http\Faking\MockResponse;
 
-it("maps GetAccounts to Account DTO list", function () {
+it('maps GetAccounts to Account DTO list', function () {
     $mockClient = new MockClient([
         GetAccounts::class => MockResponse::make(
             body: json_encode([
-                ["id" => 1, "identifier" => "one.test"],
-                ["id" => 2, "identifier" => "two.test"],
+                ['id' => 1, 'identifier' => 'one.test'],
+                ['id' => 2, 'identifier' => 'two.test'],
             ]),
             status: 200,
-            headers: ["Content-Type" => "application/json"],
+            headers: ['Content-Type' => 'application/json'],
         ),
     ]);
 
@@ -27,5 +27,5 @@ it("maps GetAccounts to Account DTO list", function () {
     expect($accounts)->toBeArray();
     expect($accounts)->toHaveCount(2);
     expect($accounts[0])->toBeInstanceOf(Account::class);
-    expect($accounts[0]->identifier)->toBe("one.test");
+    expect($accounts[0]->identifier)->toBe('one.test');
 });
